0291eb23d63857185e8774d09fc0074800dc7103
TJ Klimoski <tjklimoski@gmail.com>
2023-05-17 20:23:13 -0400

    Create deleteLetter function

--- src/App.jsx
+++ src/App.jsx
@@ -7,50 +7,68 @@ const answer = anwserWords[Math.floor(Math.random() * anwserWords.length)]
 console.log("answer: ", answer);
 
 function App() {
-  const [board, setBoard] = useState(Array(30).fill({ value: null, status: STATUS.default }));
+  const [board, setBoard] = useState(
+    Array(30).fill({ value: null, status: STATUS.default })
+  );
   const [keyboard, setKeyboard] = useState(defaultKeyboard);
+  const activeTiles = board.filter(tile => tile.status === STATUS.active);
+
+  const addLetter = useCallback(
+    (letter) => {
+      //only tiles that are in the process of being guessed will have an 'active' status. once guess is submitted the will turn to wrong, wrong-position or correct
+      if (activeTiles.length >= 5) {
+        return;
+      }
+
+      setBoard((currentBoard) => {
+        const nextEmptyPosition = currentBoard.findIndex(
+          (tile) => tile.status === STATUS.default
+        );
+        return currentBoard.map((tile, index) => {
+          if (index !== nextEmptyPosition) return tile;
+          return { value: letter, status: STATUS.active };
+        });
+      });
+    },
+    [activeTiles]
+  );
+
+  const deleteLetter = useCallback(() => {
+    const tileToDeleteIndex = activeTiles.length - 1;
+    if (tileToDeleteIndex < 0) return;
+    setBoard((currentBoard) => {
+      return currentBoard.map((tile, index) => {
+        // if value used in multiple tiles get the one in the same index position
+        if (
+          tile.value === activeTiles[tileToDeleteIndex].value &&
+          index % 5 === tileToDeleteIndex
+        )
+          return { value: null, status: STATUS.default };
+        return tile;
+      });
+    });
+  }, [activeTiles]);
 
   const handleInput = useCallback((input) => {
     input = input.toLowerCase();
-    if (input === 'delete' || input === 'backspace') return deleteLetter();
-    if (input === 'enter') return submitWord();
+    if (input === "delete" || input === "backspace") return deleteLetter();
+    if (input === "enter") return submitWord();
     if (/^[a-z]$/.test(input)) return addLetter(input);
     return;
-  }, [])
-
-  function getActiveTiles() {
-    return board.filter(tile => tile.status === STATUS.active)
-  }
-
-  function addLetter(letter) {
-
-    //only tiles that are in the process of being guessed will have an 'active' status. once guess is submitted the will turn to wrong, wrong-position or correct
-
-    setBoard(currentBoard => {
-      const nextEmptyPosition = currentBoard.findIndex(tile => tile.status === STATUS.default);
-      return currentBoard.map( (tile, index) => {
-        if (index !== nextEmptyPosition) return tile;
-        return {value: letter, status: STATUS.active}
-      })
-    })
-  }
-
-  function deleteLetter() {
-    return;
-  }
+  }, [addLetter, deleteLetter]);
 
   function submitWord() {
     return;
   }
 
+  // To allow animations to finish before allowing users to interact again
   function stopInteraction(e) {
     e.stopPropagation();
   }
 
-  //To allow animations to finish before allowing users to interact again
   function stopUserInteraction() {
     document.addEventListener("click", stopInteraction, { capture: true });
-    document.addEventListener('keydown', stopInteraction, { capture: true });
+    document.addEventListener("keydown", stopInteraction, { capture: true });
   }
 
   function restoreUserInteraction() {
@@ -58,17 +76,18 @@ function App() {
     document.removeEventListener("keydown", stopInteraction, { capture: true });
   }
 
+  // setup keydown event listener
   useEffect(() => {
     const handler = (e) => {
       handleInput(e.key);
-    }
+    };
 
     document.addEventListener("keydown", handler);
 
     return () => {
       document.removeEventListener("keydown", handler);
-    }
-  }, [handleInput])
+    };
+  }, [handleInput]);
 
   return (
     <div className="wrapper">
